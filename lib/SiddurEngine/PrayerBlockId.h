ba6f795431641316f27b39db55ecbfb7642fff66
Elad Mezrahi <30807522+emezrahi@users.noreply.github.com>
2026-09-18 16:21:24 -0400

    feat(siddur): add Amidah block ids

--- lib/SiddurEngine/PrayerBlockId.h
+++ lib/SiddurEngine/PrayerBlockId.h
@@ -6,6 +6,10 @@ enum class PrayerBlockId {
   NetilatYadayim,
   AsherYatzar,
   ElohaiNeshama,
+  RetzehOpening,
+  YaalehVeyavoRoshHodesh,
+  RetzehConclusion,
+  Modim,
 };
 
 }  // namespace SiddurEngine
