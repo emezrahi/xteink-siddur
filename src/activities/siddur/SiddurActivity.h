53e0342d055afe54508dbb4ba8e567ed50fb5019
Elad Mezrahi <30807522+emezrahi@users.noreply.github.com>
2026-09-18 14:53:01 -0400

    refactor(siddur): use bundled prayer font

--- src/activities/siddur/SiddurActivity.h
+++ src/activities/siddur/SiddurActivity.h
@@ -3,9 +3,6 @@
 #include "activities/Activity.h"
 
 class SiddurActivity final : public Activity {
-  int prayerFontId = 0;
-  bool pointedHebrewAvailable = false;
-
  public:
   explicit SiddurActivity(GfxRenderer& renderer, MappedInputManager& mappedInput)
       : Activity("Siddur", renderer, mappedInput) {}
