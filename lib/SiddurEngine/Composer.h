235226e0d058cb8310d0efcc84a632f9a4b14b52
Elad Mezrahi <30807522+emezrahi@users.noreply.github.com>
2026-09-20 00:55:36 -0400

    feat: compose ordinary weekday Shaharit

--- lib/SiddurEngine/Composer.h
+++ lib/SiddurEngine/Composer.h
@@ -13,12 +13,57 @@ class Composer final {
     if (context.service != PrayerService::Shaharit) {
       return {};
     }
+    return composeOrdinaryWeekdayShaharit(context);
+  }
 
-    return {
-        PrayerBlockId::NetilatYadayim,
-        PrayerBlockId::AsherYatzar,
-        PrayerBlockId::ElohaiNeshama,
-    };
+  [[nodiscard]] static std::vector<PrayerBlockId> composeOrdinaryWeekdayShaharit(const PrayerContext& context) {
+    if (context.service != PrayerService::Shaharit || context.weekday == Weekday::Shabbat) {
+      return {};
+    }
+
+    std::vector<PrayerBlockId> blocks;
+    blocks.reserve(24);
+
+    // ShacharitShacharGenerator.
+    blocks.push_back(PrayerBlockId::ModehAni);
+    blocks.push_back(PrayerBlockId::MorningBlessings);
+    blocks.push_back(PrayerBlockId::TorahBlessings);
+    blocks.push_back(PrayerBlockId::PetichatEliyahu);
+    blocks.push_back(PrayerBlockId::Talit);
+    blocks.push_back(PrayerBlockId::Tefillin);
+    blocks.push_back(PrayerBlockId::HannasPrayer);
+
+    // ShacharitOpeningGenerator.
+    blocks.push_back(PrayerBlockId::MorningPrayer);
+    blocks.push_back(PrayerBlockId::IncenseOfferingMorning);
+
+    // ShacharitZimraGenerator and ShacharitShmaGenerator.
+    blocks.push_back(PrayerBlockId::Hodu);
+    blocks.push_back(PrayerBlockId::PesukeiDeZimra);
+    blocks.push_back(PrayerBlockId::ShemaAndBlessings);
+
+    // Weekday Amidah. Conditional insertions are added in later rule slices.
+    blocks.push_back(PrayerBlockId::WeekdayAmidah);
+
+    const bool mondayOrThursday = context.weekday == Weekday::Monday || context.weekday == Weekday::Thursday;
+
+    // ShacharitTahanunGenerator.
+    blocks.push_back(mondayOrThursday ? PrayerBlockId::ViduiMondayThursday : PrayerBlockId::ViduiRegular);
+
+    // TorahReadingGenerator is part of ordinary Shaharit on Monday and Thursday.
+    if (mondayOrThursday) {
+      blocks.push_back(PrayerBlockId::TorahReadingWeekday);
+    }
+
+    // ShacharitSofGenerator.
+    blocks.push_back(PrayerBlockId::AshreiAfterTahanun);
+    blocks.push_back(mondayOrThursday ? PrayerBlockId::UvaLezionTorah : PrayerBlockId::UvaLezionRegular);
+    blocks.push_back(PrayerBlockId::BeitYaakov);
+    blocks.push_back(songOfDayBlock(context.weekday));
+    blocks.push_back(PrayerBlockId::Kaveh);
+    blocks.push_back(PrayerBlockId::Aleinu);
+
+    return blocks;
   }
 
   [[nodiscard]] static std::vector<PrayerBlockId> composeWeekdayAmidah(const PrayerContext& context) {
@@ -33,6 +78,27 @@ class Composer final {
     blocks.push_back(PrayerBlockId::RetzehConclusion);
     blocks.push_back(PrayerBlockId::Modim);
     return blocks;
+  }
+
+ private:
+  [[nodiscard]] static constexpr PrayerBlockId songOfDayBlock(const Weekday weekday) {
+    switch (weekday) {
+      case Weekday::Sunday:
+        return PrayerBlockId::SongOfDaySunday;
+      case Weekday::Monday:
+        return PrayerBlockId::SongOfDayMonday;
+      case Weekday::Tuesday:
+        return PrayerBlockId::SongOfDayTuesday;
+      case Weekday::Wednesday:
+        return PrayerBlockId::SongOfDayWednesday;
+      case Weekday::Thursday:
+        return PrayerBlockId::SongOfDayThursday;
+      case Weekday::Friday:
+        return PrayerBlockId::SongOfDayFriday;
+      case Weekday::Shabbat:
+        return PrayerBlockId::SongOfDaySaturday;
+    }
+    return PrayerBlockId::SongOfDaySunday;
   }
 };
 
